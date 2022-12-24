ea0d318fe7c095fc69feac8213efdeee46b3e9c4
Ali Mousavi <alimusviz99@gmail.com>
2022-12-24 15:36:42 +0330

    added main function

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 from product import Product
 
-product = Product("title", "short description", "slug", "permalink", True, "sku", 10.0, 19.0, 25.0, 10, 20)
-product.Show()
+def main():
+    product = Product("title", "short description", "slug", "permalink",
+     True, "sku", 10.0, 19.0, 25.0, 10, 20)
+    
+    product.Show()
+    product.update(slug="gtft")
+    product.delet()
+    Product.show_all()
+
+    print(type(Product))
+
+
+if __name__ == "__main__":
+    main()
